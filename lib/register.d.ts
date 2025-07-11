5071ff6e3e9863c6ce59dacd5f63f4bf082aded9
kekee000 <kekee000@gmail.com>
2025-07-11 15:17:02 +0800

    feat: add types

--- lib/register.d.ts
+++ lib/register.d.ts
@@ -1,16 +1,18 @@
-/** 
+/**
  * Hack module and fs API
  * @example
  * You can do something like this.
- * 
+ *
  * ``` js
  * require('./path/to/file.asar')
  * require('./path/to/file.asar/subdir/script.js')
  * require('./path/to/file.asar/subdir/config.json')
  * require('./path/to/file.asar/subdir/addon.node')
  * ```
- */ 
-export declare function register(): void;
+ */
+export declare function register(options?: {
+    archives?: string[];
+}): void;
 
 export declare function unregister(): void;
 
